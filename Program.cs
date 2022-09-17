026e6a5166673b821dd985c6a56afe49b6b7504e
Viviane <vpereiradasilva555@gmail.com>
2022-09-17 10:26:55 -0300

    Atividade

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int primeiroNumero, segundoNumero, soma;
+
+Console.WriteLine("Calculo da soma entre dois números.\n");
+primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo numero");
+segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+soma = primeiroNumero + segundoNumero;
+
+Console.WriteLine($"\nSoma: {soma}");
+
+
